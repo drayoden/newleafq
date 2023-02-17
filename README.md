1bba6e84c0db34902d9ec130a7ccd9b13781bd5d
daryn <me@daryn.dev>
2023-02-16 19:28:03 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# test03 (test03)
+
+test some newleaf elements
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
+```bash
+quasar dev
+```
+
+### Customize the configuration
+
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,107 @@
+<template>
+  <!-- <q-layout view="lHh Lpr lFf"> -->
+  <q-layout view="hHh lpr lFf">
+    <!-- <q-header elevated> -->
+    <!-- <q-toolbar> -->
+    <!-- right-align hamburger menu -->
+    <!-- <q-space />
+    <q-btn
+      flat
+      dense
+      round
+      icon="menu"
+      aria-label="Menu"
+      @click="toggleLeftDrawer"
+    /> -->
+    <q-page-container>
+      <router-view />
+      <q-page>
+        <q-page-sticky position="top-right" :offset="[20, 20]">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleLeftDrawer"
+          />
+        </q-page-sticky>
+      </q-page>
+    </q-page-container>
+
+    <!-- <q-toolbar-title> Quasar App </q-toolbar-title> -->
+
+    <!-- <div>Quasar v{{ $q.version }}</div> -->
+    <!-- </q-toolbar> -->
+    <!-- </q-header> -->
+
+    <q-drawer v-model="leftDrawerOpen" bordered overlay="true">
+      <q-list>
+        <q-item-label header>
+          <img
+            src="~assets/logoblack250x250.png"
+            style="width: 59px; height: 59px; margin-left: 100px"
+          />
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "home",
+    caption: "",
+    icon: "home",
+    link: "/",
+  },
+  {
+    title: "about",
+    caption: "",
+    icon: "info",
+    link: "#",
+  },
+  {
+    title: "testimonials",
+    caption: "",
+    icon: "chat",
+    link: "#",
+  },
+  {
+    title: "blog",
+    caption: "",
+    icon: "info",
+    link: "#",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
